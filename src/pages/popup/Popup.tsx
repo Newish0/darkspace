8763024336f3223e343628bec391f621b0027387
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-03-16 23:27:07 -0700

    feat: created proper popup page

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,16 +1,49 @@
-import { Button } from "@/components/ui/button";
-import { Separator } from "@kobalte/core/separator";
+import { ColorModeProvider, ColorModeScript, createLocalStorageManager } from "@kobalte/core";
+import { Separator } from "@/components/ui/separator";
+import icon from "@/../public/icon/128.png";
+import browser from "webextension-polyfill";
+import { buttonVariants } from "@/components/ui/button";
 
-export default function () {
+const VERSION = __APP_ENV__.VERSION || "unknown";
+
+const iconUrl = browser.runtime.getURL(icon);
+
+export default function Popup() {
+    const storageManager = createLocalStorageManager("vite-ui-theme");
     return (
-        <div class="top-0 bg-background text-foreground m-2 text-center w-64">
-            <h1 class="font-bold text-2xl">Darkspace</h1>
+        <>
+            <ColorModeScript storageType={storageManager.type} />
+            <ColorModeProvider storageManager={storageManager}>
+                <div class="bg-background text-foreground p-4 w-72 rounded-lg shadow-lg">
+                    <div class="flex flex-col items-center text-center">
+                        <div class="flex items-center gap-2 mb-2">
+                            <img src="/icon/128.png" alt="Darkspace Logo" class="w-10 h-10" />
+                            <h1 class="font-bold text-2xl">Darkspace</h1>
+                        </div>
 
-            <Separator />
+                        <p class="text-sm">
+                            Brightspace reimagined; an alternative frontend built with sanity in
+                            mind.
+                        </p>
+                    </div>
 
-            <p>Brightspace reimagined; an alternative frontend</p>
-
-            <Button>Test</Button>
-        </div>
+                    <div class="flex items-center justify-between mt-4 pt-2 border-t border-border">
+                        <div class="text-xs">Version: {VERSION}</div>
+                        <a
+                            href="https://github.com/Newish0/darkspace"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class={buttonVariants({
+                                variant: "link",
+                                size: "sm",
+                                class: "text-xs",
+                            })}
+                        >
+                            GitHub
+                        </a>
+                    </div>
+                </div>
+            </ColorModeProvider>
+        </>
     );
 }
